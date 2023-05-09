7d2802e2967076d9f6d69de5df12f6e5cf0dc432
Viktor <minkovvs@gmail.com>
2023-05-10 00:00:50 +0300

    feat: update Docs component

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,18 +1,17 @@
 import { Space, Typography } from 'antd';
 
-const { Title, Text } = Typography;
+const { Title, Text, Link } = Typography;
 
 const Docs = () => {
   return (
-    <Space className="docs" direction="vertical">
-      <Title level={2} className="docs__title">
-        Docs
-      </Title>
-      <Text className="docs__description">
-        A GraphQL schema provides a root type for each kind of operation
+    <Space direction="vertical">
+      <Title level={2}>Docs</Title>
+      <Text>A GraphQL schema provides a root type for each kind of operation</Text>
+      <Text>Root Types</Text>
+      <Text>
+        query:
+        <Link target="_blank">parsed queryType</Link>
       </Text>
-      <Text className="">Root Types</Text>
-      <Text className="">query:</Text>
     </Space>
   );
 };
