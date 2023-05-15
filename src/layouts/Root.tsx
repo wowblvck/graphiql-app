6015afa2f07043184e0f15b61d5f25fa6b878c8d
Indar Basto <indar.basto@gmail.com>
2023-05-15 14:02:10 +0300

    feat: add page transition effect

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useLocation, useOutlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import './Root.css';
 
 const Root = () => {
+  const location = useLocation();
+  const currentOutlet = useOutlet();
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Header />
-      <Outlet />
+      <SwitchTransition>
+        <CSSTransition key={location.pathname} classNames="fade" timeout={300} unmountOnExit>
+          {currentOutlet}
+        </CSSTransition>
+      </SwitchTransition>
       <Footer />
     </Layout>
   );
