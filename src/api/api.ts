4c4326ebf91e0aa2f459b390551ff3488ce14b98
Viktor <minkovvs@gmail.com>
2023-05-28 22:43:29 +0300

    feat: add error handling and popup for it, add translation

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,31 +1,5 @@
 import { BASE_URL } from '@/constants/settings.config';
-import { IntrospectionType } from '@/types/api.types';
-import {
-  buildASTSchema,
-  buildClientSchema,
-  getIntrospectionQuery,
-  parse,
-  printSchema,
-} from 'graphql';
 import { GraphQLClient, gql } from 'graphql-request';
-
-export const fetchGraphQLSchema = async () => {
-  try {
-    const response = await fetch(BASE_URL, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({
-        query: getIntrospectionQuery(),
-        operationName: 'IntrospectionQuery',
-      }),
-    });
-    const { data }: IntrospectionType = await response.json();
-    const schema = buildClientSchema(data);
-    return buildASTSchema(parse(printSchema(schema)));
-  } catch (error) {
-    console.error(`fetchGraphQLSchema => ${error}`);
-  }
-};
 
 export const fetchGraphQLQuery = async (queryStr: string, variables?: string) => {
   try {
@@ -38,11 +12,11 @@ export const fetchGraphQLQuery = async (queryStr: string, variables?: string) =>
       ${queryStr}
     `;
 
-    const vars = variables ? JSON.parse(variables) : {};
+    const vars = variables ? JSON.parse(variables) : null;
 
     const data = await graphQLClient.request(query, vars);
     return data;
-  } catch (e) {
-    return e;
+  } catch (error) {
+    return error;
   }
 };
